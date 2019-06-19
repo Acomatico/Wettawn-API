fc927cba9f1dcf682546c2aa670809e1bd8faaa7
bos32 <acomatico18@gmail.com>
2019-06-19 17:35:04 +0200

    commit heroku 2

--- database/mongo-pool.js
+++ database/mongo-pool.js
@@ -1,6 +1,7 @@
 'use strict';
 const mongoose = require('mongoose')
 const mongoUri = process.env.MONGO_ATLAS_STRING;
+// const mongoUri = process.env.MONGO_URI;
 async function connect() {
     try {
         const connection = await mongoose.connect(mongoUri, { useNewUrlParser: true })
